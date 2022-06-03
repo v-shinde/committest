922df5fb7a7ecf962f10d8fb8a9ed3c1f72bf3d2
Vaibhav Shinde <vaibhav.shinde@pinnacleit.in>
2022-06-03 17:03:58 +0530

    added graphql query

--- file1.js
+++ file1.js
@@ -5,3 +5,36 @@ console.log('Second Commit');
 console.log('Second Commit');
 
 console.log('Fourth Commit');
+
+{
+    repository(owner:"v-shinde", name:"committest") {
+      refs(first: 100, refPrefix: "refs/heads/") {
+          totalCount
+        edges {
+          node {
+            name
+            target {
+              ... on Commit {
+                id
+                history(first: 1) {
+                  pageInfo{
+                    hasNextPage
+                    startCursor
+                    endCursor
+                  }
+                  totalCount
+                  nodes {
+                    oid
+                    message
+                    additions
+                    deletions
+                    changedFiles
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }`
